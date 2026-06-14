41ea3f9d4f1db5bb51da5141ee722f63b54bcff3
programmer-2025 <snowsansub011307@gmail.com>
2026-06-14 20:46:46 +0900

    ＜追加＞　ログインすることができるようになりました

--- php/main.php
+++ php/main.php
@@ -1,16 +1,22 @@
 <?php
-    echo 'aa';
     $db_connect = 'mysql:host=localhost; dbname=test; charset=utf8';
+    $input_name = $_POST['name'];
+    $input_pass = $_POST['password'];
 
     try {
         $pdo = new PDO($db_connect, 'aaa1', 'password');
 
-        $sql = "SELECT * FROM user_testtable;";
+        $sql = "SELECT * FROM user_testtable WHERE name = :name AND password = :password;";
         $stmt = $pdo->prepare($sql);
-        $stmt->execute();
+        $stmt->execute([':name' => $input_name, ':password' => $input_pass]);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        print_r($row);
+        if ($row) {
+            echo 'ログインすることができました。';
+        }
+        else {
+            echo 'ログインできませんでした。';
+        }
     } catch(PDOException $e) {
         echo $e->getMessage();
     }
